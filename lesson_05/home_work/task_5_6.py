48d2d768a0f4d0f04127e52780148a45f88a25d5
AndreiKotM <andrew.m.kot@gmail.com>
2020-07-15 17:08:26 +0300

    task_5_6 is started

--- lesson_05/home_work/task_5_6.py
+++ lesson_05/home_work/task_5_6.py
@@ -1,3 +1,15 @@
 # Задан целочисленный массив. Определить количество участков массива,
 # на котором элементы монотонно возрастают (каждое следующее число
 # больше предыдущего).
+
+def counter(input_list):
+    tmp = input_list[0]
+
+    for i in range(len(input_list)):
+        tmp = input_list[i]
+        j = i + 1
+
+        if i < len(input_list) - 1:
+            while j < len(input_list):
+                if tmp < input_list[j]:
+                    pass
